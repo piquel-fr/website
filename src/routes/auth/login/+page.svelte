9a80b15e674e407ff96cbeacbb8c833a82e46687
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-03-01 09:35:38 +0100

    Login: Made page with both providers

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,1 +1,39 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import { PUBLIC_API } from "$env/static/public";
+    import Card from "$lib/components/Card.svelte";
+    import NavButton from "$lib/components/NavButton.svelte";
+
+    import google from "$lib/svg/google.svg";
+    import github from "$lib/svg/github.svg";
+</script>
+
+<div class="flex justify-center items-center m-12">
+    <Card
+        popOut={false}
+        className="flex flex-col justify-center items-center gap-3 p-5 m-1"
+    >
+        <p class="text-3xl">Login</p>
+        <p class="text-xl">Select a provider to login with</p>
+
+        <NavButton
+            dest={`${PUBLIC_API}/auth/github?redirectTo=/`}
+            popOut={false}
+            className="px-5 py-3 border flex gap-2
+                border-slate-200 rounded-lg text-slate-700 hover:border-slate-400
+                hover:text-slate-900 hover:shadow transition duration-150"
+        >
+            <img src={github} alt="github" class="size-8" />
+            <span class="text-2xl">Login with GitHub</span>
+        </NavButton>
+        <NavButton
+            dest={`${PUBLIC_API}/auth/google?redirectTo=/`}
+            popOut={false}
+            className="px-5 py-3 border flex gap-2
+                    border-slate-200 rounded-lg text-slate-700 hover:border-slate-400
+                    hover:text-slate-900 hover:shadow transition duration-150"
+        >
+            <img src={google} alt="google" class="size-8" />
+            <span class="text-2xl">Login with Google</span>
+        </NavButton>
+    </Card>
+</div>
