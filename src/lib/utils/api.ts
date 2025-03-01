8b0bc53566a9ebd1eeb7d596dd7ba26ac51d43fe
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-03-01 14:34:34 +0100

    API: Fixed error where json would be resolved even if not a json response

--- src/lib/utils/api.ts
+++ src/lib/utils/api.ts
@@ -5,15 +5,16 @@ export const fetchAPI = async (
     { fetch }: LoadEvent,
     path: string,
 ): Promise<{ data: any; status: number }> => {
-    const response = await fetch(
-        `${PUBLIC_API}${path}`,
-        {
-            credentials: "include",
-        },
-    );
+    const response = await fetch(`${PUBLIC_API}${path}`, {
+        credentials: "include",
+    });
 
-    const status = response.status;
-    const data = response.json();
+    if (response.headers.get("Content-Type") == "application/json") {
+        return {
+            data: response.json(),
+            status: response.status,
+        };
+    }
 
-    return { data, status };
+    return { data: {}, status: response.status };
 };
