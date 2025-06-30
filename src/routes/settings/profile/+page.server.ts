47b72b4eb80f3a84fc066d67f5bccc415287d1c4
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-06-30 20:33:49 +0200

    setup updating profile (needs styling)

---
+++ src/routes/settings/profile/+page.server.ts
@@ -0,0 +1,27 @@
+import { PUBLIC_API } from "$env/static/public";
+import { error } from "@sveltejs/kit";
+import type { Actions } from "./$types";
+
+export const actions = {
+    default: async ({ request, fetch }) => {
+        const data = await request.formData();
+
+        const response = await fetch(`${PUBLIC_API}/profile`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+            method: "PUT",
+            body: JSON.stringify({
+                username: data.get("username"),
+                name: data.get("name"),
+                image: data.get("image"),
+            }),
+        });
+
+        if (response.status != 200) {
+            console.log(response);
+            error(response.status, await response.text());
+        }
+    },
+} satisfies Actions;
--- src/routes/settings/profile/+page.svelte
+++ src/routes/settings/profile/+page.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
+    import Button from "$lib/components/Button.svelte";
     import type { PageProps } from "./$types";
 
     let { data }: PageProps = $props();
     let profile = data.settings.profile;
+
+    let username: string = $state(profile.username);
+    let name: string = $state(profile.name);
+    let image: string = $state(profile.image);
 </script>
+
+<form id="update-profile" method="POST">
+    <input bind:value={username} name="username" type="text" />
+    <input bind:value={name} name="name" type="text" />
+    <input bind:value={image} name="image" type="text" />
+
+    <Button type="submit" form="update-profile">Save</Button>
+</form>
