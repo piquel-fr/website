773fe3912cc8ce810ab2cad9855016e8e6b53d5e
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-07-21 17:10:48 +0200

    style the settings page

--- src/routes/settings/profile/+page.svelte
+++ src/routes/settings/profile/+page.svelte
@@ -10,10 +10,36 @@
     let image: string = $state(profile.image);
 </script>
 
-<form id="update-profile" method="POST">
-    <input bind:value={username} name="username" type="text" />
-    <input bind:value={name} name="name" type="text" />
-    <input bind:value={image} name="image" type="text" />
+<form id="update-profile" method="POST" class="flex flex-col">
+    <label class="mb-1">
+        <p class="text-sm text-gray-600">Username</p>
+        <input
+            class="rounded-md border-gray-400 bg-gray-100 border-2 p-1"
+            bind:value={username}
+            name="username"
+            type="text"
+        />
+    </label>
 
-    <Button type="submit" form="update-profile">Save</Button>
+    <label class="mb-1">
+        <p class="text-sm text-gray-600">Name</p>
+        <input
+            class="rounded-md border-gray-400 bg-gray-100 border-2 p-1"
+            bind:value={name}
+            name="name"
+            type="text"
+        />
+    </label>
+
+    <label class="mb-1">
+        <p class="text-sm text-gray-600">Image</p>
+        <input
+            class="rounded-md border-gray-400 bg-gray-100 border-2 p-1"
+            bind:value={image}
+            name="image"
+            type="text"
+        />
+    </label>
+
+    <Button className="mt-2" type="submit" form="update-profile">Save</Button>
 </form>
