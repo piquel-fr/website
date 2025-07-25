14f016bae359d077a349fcd6ae6166cc65a92672
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-07-25 16:50:12 +0200

    add logging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,15 @@
 import type { Handle } from "@sveltejs/kit";
 
 export const handle = (({ event, resolve }) => {
+    console.log("Request headers:", {
+        origin: event.request.headers.get("origin"),
+        host: event.request.headers.get("host"),
+        "x-forwarded-host": event.request.headers.get("x-forwarded-host"),
+        "x-forwarded-proto": event.request.headers.get("x-forwarded-proto"),
+        referer: event.request.headers.get("referer"),
+    });
+    console.log("Environment ORIGIN:", "https://piquel.fr");
+
     return resolve(event, {
         // Forward all headers
         filterSerializedResponseHeaders: () => true,
