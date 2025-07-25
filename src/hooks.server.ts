395ba8a3d6de9ec4892de3e6af6ef33991c4e4f8
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-07-25 16:42:21 +0200

    remove debug & origin checking

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,6 @@
-import { ORIGIN } from "$env/static/private";
 import type { Handle } from "@sveltejs/kit";
 
 export const handle = (({ event, resolve }) => {
-    console.log(ORIGIN);
     return resolve(event, {
         // Forward all headers
         filterSerializedResponseHeaders: () => true,
