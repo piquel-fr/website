4eb7bbe6bfc71ad4486a68942ad283032fc104f9
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-08-13 17:59:23 +0200

    add better cookie settings

--- src/routes/auth/login/callback/+server.ts
+++ src/routes/auth/login/callback/+server.ts
@@ -4,10 +4,10 @@ export const GET: RequestHandler = ({ cookies, url }) => {
     const token = url.searchParams.get("token") || "";
     cookies.set("jwt", token, {
         path: "/",
-        // httpOnly: false,
-        // secure: true,
-        // sameSite: "none",
-        // maxAge: 178200,
+        httpOnly: false,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 178200,
     });
 
     const redirectTo = url.searchParams.get("redirectTo") || "";
