7ebd39cb3e4226cc54f8d2f17580f934495690c5
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-08-13 17:17:46 +0200

    fix cookie storage parameters

--- src/routes/auth/login/callback/+server.ts
+++ src/routes/auth/login/callback/+server.ts
@@ -2,7 +2,12 @@ import { redirect, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = ({ cookies, url }) => {
     const token = url.searchParams.get("token") || "";
-    cookies.set("jwt", token, { path: "/" });
+    cookies.set("jwt", token, {
+        path: "/",
+        httpOnly: false,
+        secure: true,
+        sameSite: "none",
+    });
 
     const redirectTo = url.searchParams.get("redirectTo") || "";
     redirect(307, redirectTo);
