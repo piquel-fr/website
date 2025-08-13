b5225fc96501b461c9e137410d635991bd7dc1e9
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-08-13 17:50:47 +0200

    remove a bunch of stuff in cookies

--- src/routes/auth/login/callback/+server.ts
+++ src/routes/auth/login/callback/+server.ts
@@ -3,12 +3,11 @@ import { redirect, type RequestHandler } from "@sveltejs/kit";
 export const GET: RequestHandler = ({ cookies, url }) => {
     const token = url.searchParams.get("token") || "";
     cookies.set("jwt", token, {
-        domain: "api.piquel.fr",
         path: "/",
-        httpOnly: false,
-        secure: true,
-        sameSite: "none",
-        maxAge: 178200,
+        // httpOnly: false,
+        // secure: true,
+        // sameSite: "none",
+        // maxAge: 178200,
     });
 
     const redirectTo = url.searchParams.get("redirectTo") || "";
