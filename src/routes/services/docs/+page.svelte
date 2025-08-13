4ec82fdb73cdebf294f72c018bbbd77c471aab5f
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-08-13 15:17:37 +0200

    add buttons to create new docs service

--- src/routes/services/docs/+page.svelte
+++ src/routes/services/docs/+page.svelte
@@ -2,6 +2,8 @@
     import type { PageProps } from "./$types";
     import { page } from "$app/state";
     import DocumentationInstance from "$lib/components/services/docs/DocumentationInstance.svelte";
+    import NavButton from "$lib/components/NavButton.svelte";
+    import plus from "$lib/svg/plus.svg";
 
     let { data }: PageProps = $props();
 
@@ -24,10 +26,27 @@
     );
 </script>
 
-<h1 class="text-xl">Your Documentation Instances</h1>
-{#each data.docs.instances as doc}
-    <DocumentationInstance {doc} />
-{/each}
+<div class="flex mr-0.5">
+    <h1 class="text-xl mr-auto">Your Documentation Instances</h1>
+    <NavButton className="p-1 bg-gray-200" dest="/services/docs/new"
+        ><img src={plus} alt="New" class="size-5" /></NavButton
+    >
+</div>
+<div class="mt-4 mb-3">
+    {#if data.docs.count == 0}
+        <NavButton
+            dest="/services/docs/new"
+            popOut={false}
+            className="w-full my-1 p-4"
+        >
+            <img src={plus} alt="New" class="size-20 mx-auto" />
+            <p class="w-fit mx-auto">Create your first instance</p>
+        </NavButton>
+    {/if}
+    {#each data.docs.instances as doc}
+        <DocumentationInstance {doc} />
+    {/each}
+</div>
 <div class="grid grid-cols-3 pt-0.5 mx-1">
     <a
         class="hover:text-gray-600 size-fit"
