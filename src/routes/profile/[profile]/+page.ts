acb9f6007b003780ec480d431d430d3df4047cea
Piquel <piquel@piquel.fr>
2025-11-24 15:27:34 +0100

    Remove trailing slash from profile API endpoint

--- src/routes/profile/[profile]/+page.ts
+++ src/routes/profile/[profile]/+page.ts
@@ -3,6 +3,6 @@ import api from "$lib/utils/api";
 import type { LoadEvent } from "@sveltejs/kit";
 
 export const load: PageLoad = async ({ fetch, url, params }: LoadEvent) => {
-    const response = await api.get(fetch, url, `/profile/${params.profile}/`);
+    const response = await api.get(fetch, url, `/profile/${params.profile}`);
     return { profileData: await response.data };
 };
