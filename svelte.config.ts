bd06b8406069e664ba1aa002ac7c7a97640f53f0
PiquelChips <piquel@piquel.fr>
2026-01-18 13:52:07 +0100

    rename svelte config to be .ts

--- svelte.config.ts
+++ svelte.config.ts
@@ -3,7 +3,7 @@ import adapter from "svelte-adapter-deno";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
     // Consult https://svelte.dev/docs/kit/integrations
     // for more information about preprocessors
     preprocess: [vitePreprocess(), mdsvex()],
@@ -11,5 +11,3 @@ const config = {
     kit: { adapter: adapter() },
     extensions: [".svelte", ".svx"]
 };
-
-export default config;
