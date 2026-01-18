59fdda5253565f38b6317799ae72bda9cc091125
PiquelChips <piquel@piquel.fr>
2026-01-18 15:36:30 +0100

    add main docs layout

---
+++ src/routes/docs/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import type { NavTree } from "$types/docs";
+    import Nav from "$lib/components/docs/Nav.svelte";
+
+    let { children } = $props();
+
+    const nav: NavTree[] = [
+        {
+            name: "Home",
+            path: "/docs",
+        },
+        {
+            name: "API",
+            path: "/docs/api",
+            children: [
+                { name: "Auth", path: "/docs/api/auth" },
+                { name: "Profile", path: "/docs/api/profile" },
+                { name: "Email", path: "/docs/api/email" },
+            ],
+        },
+        { name: "Docs", path: "/docs/docs" },
+    ];
+</script>
+
+<div class="flex">
+    <Nav nodes={nav}></Nav>
+    <div>
+        {@render children()}
+    </div>
+</div>
