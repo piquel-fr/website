1723ad5feb70d21d03644098ebd26014466342c6
PiquelChips <piquel@piquel.fr>
2026-01-19 17:44:18 +0100

    add layout to mdsvec config

--- svelte.config.ts
+++ svelte.config.ts
@@ -2,6 +2,12 @@ import { mdsvex } from "mdsvex";
 import adapter from "svelte-adapter-deno";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+// temporary. waiting on https://github.com/pngwn/MDsveX/pull/735
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('@sveltejs/kit').Config} */
 export default {
     // Consult https://svelte.dev/docs/kit/integrations
@@ -9,7 +15,8 @@ export default {
     preprocess: [
         vitePreprocess(),
         mdsvex({
-            extensions: [".svx", ".md"]
+            extensions: [".svx", ".md"],
+            layout: join(__dirname, "./src/lib/components/docs/DocsLayout.svelte")
         })
     ],
 
