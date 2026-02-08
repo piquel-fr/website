c2dddd4cc5e1a7664b2b9f064affc82d5742d9cb
PiquelChips <piquel@piquel.fr>
2026-02-08 19:09:53 +0100

    update cookie forwarding for new auth system

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,14 @@
-import type { Handle, HandleFetch } from "@sveltejs/kit";
+import { PUBLIC_API } from "$env/static/public";
+import type { HandleFetch } from "@sveltejs/kit";
 
-export const handle = (({ event, resolve }) => {
-    return resolve(event, {
-        // Forward all headers
-        filterSerializedResponseHeaders: () => true,
-    });
-}) satisfies Handle;
+export const handleFetch: HandleFetch = ({ request, event, fetch }) => {
+    if (request.url.startsWith(PUBLIC_API)) {
+        const cookies = event.request.headers.get("cookie");
 
-export const handleFetch: HandleFetch = ({ event, request, fetch }) => {
-    request.headers.set("Authorization", `Bearer ${event.cookies.get("jwt")}`);
+        if (cookies) {
+            request.headers.set("cookie", cookies);
+        }
+    }
+
     return fetch(request);
 };
