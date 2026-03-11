9e9fc82a97820b0041441e4e0cab7b6f7cee427c
koprulukings-beep <koprulukings@gmail.com>
2026-03-11 11:18:56 +0100

    -msg

--- src/routes/(app)/settings/mail/new/+page.svelte
+++ src/routes/(app)/settings/mail/new/+page.svelte
@@ -48,7 +48,7 @@
 
 <div class="w-full max-w-3xl mx-auto flex flex-col py-8">
     <NavButton
-        className="mb-6 p-2 px-4 rounded text-black hover:text-black dark:text-gray-300 dark:hover:text-gray-100"
+        className="mb-6 p-2 px-4 rounded text-black hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100"
         dest="/settings/mail"
         popOut={false} 
     >
