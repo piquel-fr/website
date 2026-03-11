f616bc20bede7aff74b74acffd1798197ed05741
koprulukings-beep <koprulukings@gmail.com>
2026-03-11 13:54:41 +0100

    g

--- src/routes/(app)/settings/mail/[id]/edit/+page.svelte
+++ src/routes/(app)/settings/mail/[id]/edit/+page.svelte
@@ -83,7 +83,7 @@
                     placeholder="Username"
                     bind:value={newUserInput}
                     onkeypress={(e) => e.key === "Enter" && addUser()}
-                    class="flex-1 px-4 py-2 border rounded-lg bg-white dark:bg-gray-700 border-gray-300 dark:border-gray-600 text-gray-600 dark:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    class="w-full px-4 py-2 border rounded-lg bg-white dark:bg_gray-700 border-gray-300 dark:border-gray-600 text-gray-900 dark:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
                 <button
                     onclick={addUser}
